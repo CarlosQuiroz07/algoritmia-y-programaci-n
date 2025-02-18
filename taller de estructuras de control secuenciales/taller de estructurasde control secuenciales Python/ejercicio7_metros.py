b172c7cca8fcea09e05505cce9de8ae203d49e52
CarlosQuiroz07 <carlosantiago0304@gmail.com>
2025-02-18 16:35:28 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/taller de estructurasde control secuenciales Python/ejercicio7_metros.py
@@ -0,0 +1,9 @@
+
+#entrada
+metros=float(input("ingrese los metros que desea convertir a pies y a pulgadas"))
+#caja negra 
+pulgadas=(metros*39.27)
+pies=(pulgadas/12)
+#salida
+print("el resultado en conversion de metros a pulgadas es:", pulgadas)
+print("el resultado de conversion a pies:", pies)
