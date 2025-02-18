48aa33649e68cee3108bb52e11159264d4c61333
CarlosQuiroz07 <carlosantiago0304@gmail.com>
2025-02-18 16:34:01 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/taller de estructurasde control secuenciales Python/ejercicio2_interes_mensual.py
@@ -0,0 +1,8 @@
+
+#entrada
+inversion=int(input("ingrese su inversion"))
+#caja negra
+inversion1=(inversion*0.02)
+#salida
+print("el dinero ganado es:", inversion1)
+print("el dinero total es", inversion1+inversion )
