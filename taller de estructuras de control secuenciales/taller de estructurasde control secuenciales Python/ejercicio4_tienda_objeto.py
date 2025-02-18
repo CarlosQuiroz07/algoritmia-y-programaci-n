6efe6afb453931265182ece833665106351ea265
CarlosQuiroz07 <carlosantiago0304@gmail.com>
2025-02-18 16:34:52 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/taller de estructurasde control secuenciales Python/ejercicio4_tienda_objeto.py
@@ -0,0 +1,8 @@
+#entrada
+objeto=int(input("ingrese el precio de su producto"))
+#caja negra
+valor_neto=(objeto*0.15)-objeto
+#salidas
+print("ingrese el valor del descuento del 15% es:", objeto*0.15)
+print("ingrese el valor total del producto es:", valor_neto)
+
