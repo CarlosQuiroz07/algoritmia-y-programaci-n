5bee7b3bfe1beea911dd643379617d7cbf26149b
CarlosQuiroz07 <carlosantiago0304@gmail.com>
2025-02-24 11:19:42 -0500

    Create ejercicio_3_datos.py

---
+++ Taller de estructuras de control: Selectivas/ejercicio_3_datos.py
@@ -0,0 +1,9 @@
+#entrada
+a=int(input("valor de a?:"))
+b=int(input("valor de b?:"))
+c=int(input("valor de c?:"))
+d=int(input("valor de d?:"))
+if d==0:
+    print((a-c)**2)
+elif d>0:
+    print(((a-b)**3)/d)
